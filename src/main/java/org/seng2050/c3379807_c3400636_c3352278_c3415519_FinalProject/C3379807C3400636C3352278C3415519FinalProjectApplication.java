cf4c1cbdb15c3fdfb489af839ce8e55060b8fbf9
Patrick Holt <44394183+Pholty@users.noreply.github.com>
2024-05-21 18:36:06 +1000

    Set up Spring Boot Project

---
+++ src/main/java/org/seng2050/c3379807_c3400636_c3352278_c3415519_FinalProject/C3379807C3400636C3352278C3415519FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.seng2050.c3379807_c3400636_c3352278_c3415519_FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C3379807C3400636C3352278C3415519FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C3379807C3400636C3352278C3415519FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/seng2050/c3379807_c3400636_c3352278_c3415519_FinalProject/C3379807C3400636C3352278C3415519FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seng2050.c3379807_c3400636_c3352278_c3415519_FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C3379807C3400636C3352278C3415519FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
