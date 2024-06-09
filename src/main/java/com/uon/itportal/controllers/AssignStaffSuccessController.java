3b8810120c1471487597a899f1874f7b2468ae3f
tobytrnn <tobytrnn@gmail.com>
2024-06-09 17:35:15 +1000

    Some controllers upddate

--- src/main/java/com/uon/itportal/controllers/AssignStaffSuccessController.java
+++ src/main/java/com/uon/itportal/controllers/AssignStaffSuccessController.java
@@ -1,16 +1,14 @@
+
 package com.uon.itportal.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AssignStaffSuccessController {
 
     @GetMapping("/assign-staff-success")
-    public String assignStaffSuccessMessage(Model model) {
-        // Add a success message to the model
-        model.addAttribute("message", "Staff successfully assigned to the issue.");
+    public String newIssueSuccessMessage() {
         return "assign_staff_success";
     }
 }
